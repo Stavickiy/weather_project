9e136af685863013dc69d4c333562eecd8b9d9fa
Stavickiy <stavickiydgo@gmail.com>
2024-07-27 12:22:14 +0200

    Finished developing!

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from api_v1 import views
 
@@ -7,4 +5,4 @@ app_name = "api_v1"
 
 urlpatterns = [
     path('weather/', views.GetForecastWeatherAPIView.as_view(), name='weather'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
